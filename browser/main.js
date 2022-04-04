10897f7a48ef7d44395dc32a42be375d2827354f
Loki <sneyed@gmail.com>
2022-04-04 21:07:29 +1000

    convert string to bytes and back again

--- browser/main.js
+++ browser/main.js
@@ -4,15 +4,30 @@ let ws = null;
 
 function onConnectButtonClick() {
   ws = new WebSocket("ws://localhost:3044");
-
-  ws.onopen = function () {
-    console.log("Connected to localhost:3044");
-  };
+  ws.addEventListener("open", onConnect);
+  ws.addEventListener("message", onMessageReceived);
 }
 
 function onSendMessageButtonClick() {
   console.log('Sending "Hello there!"');
-  ws.send("Hello there!");
+  ws.send(stringToByteArray("Hello there!"));
+}
+
+function onConnect() {
+  console.log("Connected to localhost:3044");
+}
+
+async function onMessageReceived(event) {
+  const message = await event.data.text();
+  console.log("Received:", message);
+}
+
+function stringToByteArray(str) {
+  const bytes = new Uint8Array(str.length);
+  for (let i = 0; i < str.length; i++) {
+    bytes[i] = str.charCodeAt(i);
+  }
+  return bytes;
 }
 
 function init() {
